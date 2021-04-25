d80bfa6074c336150b99097ca0d5a0d51b3f5f5f
tresende <thiago.gcresende@gmail.com>
2021-04-25 20:17:56 -0300

    randomzie dices

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -5,25 +5,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 0
-    var rigthDiceNumber = 5
-    var max = 5
-    var min = 0
-    var step = 1
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView1.image = diceArray[leftDiceNumber]
-        diceImageView2.image = diceArray[rigthDiceNumber]
-        
-        leftDiceNumber  = leftDiceNumber >= max ? min : leftDiceNumber + step
-        rigthDiceNumber = rigthDiceNumber <= min ? max : rigthDiceNumber - step
-    
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+
     }
 }
 
